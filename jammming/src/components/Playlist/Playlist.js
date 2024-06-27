ee268880a078c87f5cb45e9899f6a4afa410eed4
JordanBandur <jordanbandur@hotmail.ca>
2024-06-27 10:50:03 -0700

    Remove comment

--- jammming/src/components/Playlist/Playlist.js
+++ jammming/src/components/Playlist/Playlist.js
@@ -14,10 +14,10 @@ class Playlist extends React.Component {
 
   render() {
     return (
-      <div className="Playlist"> {/* Updated class name */}
+      <div className="Playlist">
         <input defaultValue={'New Playlist'} onChange={this.handleNameChange} />
         <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true} />
-        <button className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</button> {/* Updated to button */}
+        <button className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</button>
       </div>
     );
   }
