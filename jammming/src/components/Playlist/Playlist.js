4a23604959931d76a238c830da59a22b0f7113c1
JordanBandur <jordanbandur@hotmail.ca>
2024-06-27 10:47:29 -0700

    Update Playlist.js

--- jammming/src/components/Playlist/Playlist.js
+++ jammming/src/components/Playlist/Playlist.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './Playlist.css';
-
 import TrackList from '../TrackList/TrackList';
 
 class Playlist extends React.Component {
   constructor(props) {
     super(props);
-    
     this.handleNameChange = this.handleNameChange.bind(this);
   }
 
@@ -16,10 +14,10 @@ class Playlist extends React.Component {
 
   render() {
     return (
-      <div className="New Playlist">
+      <div className="Playlist"> {/* Updated class name */}
         <input defaultValue={'New Playlist'} onChange={this.handleNameChange} />
         <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true} />
-        <a className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</a>
+        <button className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</button> {/* Updated to button */}
       </div>
     );
   }
