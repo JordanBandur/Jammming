cfad55875111e2ed621c6a76e81fa0a51c52465a
JordanBandur <jordanbandur@hotmail.ca>
2024-06-27 11:04:11 -0700

    Fix project structure

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app/App';
 import registerServiceWorker from './registerServiceWorker';
+require('dotenv').config();
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
